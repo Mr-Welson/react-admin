e35ac41cfe8b214c1e9a1a62b810fb5f915724ba
welson <iwelson@foxmail.com>
2021-04-14 09:45:38 +0800

    修改 GlobalFooter 组件

--- src/layouts/basic/components/GlobalFooter/index.js
+++ src/layouts/basic/components/GlobalFooter/index.js
@@ -1,5 +1,13 @@
+import React from 'react';
+import './index.less';
+
 const GlobalFooter = () => {
-  return null
+  return (
+    <div className="global-footer">
+      {/* Ant Design ©2018 Created by Ant UED */}
+      中孚信息股份有限公司
+    </div>
+  )
 }
 
 export default GlobalFooter
